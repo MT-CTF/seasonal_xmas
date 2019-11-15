91e5eee2c96372de5a03de27cd5a681291e4bc62
LoneWolfHT <lonewolf04361@gmail.com>
2019-11-14 19:25:42 -0800

    Make requested changes

--- snow/init.lua
+++ snow/init.lua
@@ -1,9 +1,9 @@
-ctf_match.register_on_build_time_start(function()
+ctf_match.register_on_new_match(function()
 	minetest.add_particlespawner{
 		amount = 100,
 		time = 0,
-		minpos = vector.new(ctf_map.map.pos1.x - 1, ctf_map.map.h/2 - 1, ctf_map.map.pos1.z - 1),
-		maxpos = vector.new(ctf_map.map.pos2.x - 1, ctf_map.map.h/2 - 1, ctf_map.map.pos2.z - 1),
+		minpos = vector.new(ctf_map.map.pos1.x, ctf_map.map.h/2 - 1, ctf_map.map.pos1.z),
+		maxpos = vector.new(ctf_map.map.pos2.x, ctf_map.map.h/2 - 1, ctf_map.map.pos2.z),
 		minvel = {x=0, y=-10, z=0},
 		maxvel = {x=0, y=-15, z=0},
 		minacc = {x=0, y=0, z=0},
