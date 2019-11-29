8fa64426ad617313992fb0ed0ac583c6130e4e4c
LoneWolfHT <lonewolf04361@gmail.com>
2019-11-28 17:31:06 -0800

    Revert replacing particlespawner when match starts

--- snow/init.lua
+++ snow/init.lua
@@ -3,15 +3,11 @@ local particlespawners = {}
 local function attach_snow_spawner(player)
 	local name = player:get_player_name()
 
-	if particlespawners[name] ~= nil then
-		minetest.delete_particlespawner(particlespawners[name])
-	end
-
 	particlespawners[name] = minetest.add_particlespawner{
 		amount = math.random(50, 100),
 		time = 0,
-		minpos = vector.new(-50, 20, -50),
-		maxpos = vector.new(50, 25, 50),
+		minpos = vector.new(-17, 20, -17),
+		maxpos = vector.new(17, 25, 17),
 		minvel = {x = 0, y = -10, z = 0},
 		maxvel = {x = 0, y = -15, z = 0},
 		minacc = vector.new(),
@@ -30,14 +26,6 @@ local function attach_snow_spawner(player)
 		glow = 0
 	}
 end
-
-ctf_match.register_on_new_match(function() -- Attach snow spawner to all online players
-	minetest.after(5, function()
-		for _, player in pairs(minetest.get_connected_players()) do
-			attach_snow_spawner(player)
-		end
-	end)
-end)
 
 minetest.register_on_joinplayer(function(player) -- Attach snow spawner to player
 	attach_snow_spawner(player)
