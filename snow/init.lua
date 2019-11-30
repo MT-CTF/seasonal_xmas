19c868a44884bfd1517e1daefb3b80bf899b6b0b
LoneWolfHT <lonewolf04361@gmail.com>
2019-11-30 10:59:36 -0800

    Switch to spawning particles with a globalstep

--- snow/init.lua
+++ snow/init.lua
@@ -1,39 +1,42 @@
-local particlespawners = {}
-
-local function attach_snow_spawner(player)
-	local name = player:get_player_name()
-
-	particlespawners[name] = minetest.add_particlespawner{
-		amount = math.random(50, 100),
-		time = 0,
-		minpos = vector.new(-17, 20, -17),
-		maxpos = vector.new(17, 25, 17),
-		minvel = {x = 0, y = -10, z = 0},
-		maxvel = {x = 0, y = -15, z = 0},
-		minacc = vector.new(),
-		maxacc = vector.new(),
-		minexptime = 9,
-		maxexptime = 10,
-		minsize = 1,
-		maxsize = 4,
-		collisiondetection = true,
-		collision_removal = true,
-		object_collision = true,
-		vertical = false,
-		playername = name,
-		attached = player,
-		texture = "snow_snowflake.png",
-		glow = 0
-	}
+local function spawn_particle_inside_area(player, pos1, pos2) -- Spawns a particle inside the area defined by pos1/pos2
+	minetest.add_particle({
+        pos = {
+			x = math.random(pos1.x, pos2.x),
+			y = math.random(pos1.y, pos2.y),
+			z = math.random(pos1.z, pos2.z),
+		},
+        velocity = vector.new(0, math.random(-10, -15), 0),
+        expirationtime = 10,
+        size = math.random(3, 5),
+        collisiondetection = true,
+        collision_removal = true,
+        object_collision = true,
+        vertical = false,
+        texture = "snow_snowflake.png",
+        playername = player:get_player_name(),
+        glow = 0
+	})
 end
 
-minetest.register_on_joinplayer(function(player) -- Attach snow spawner to player
-	attach_snow_spawner(player)
-end)
+local function spawn_particles(player) -- Spawns 7 snow particles around player
+	local pos = player:get_pos()
+	local pos1 = vector.new(pos.x - 20, pos.y + 15, pos.z - 20)
+	local pos2 = vector.new(pos.x + 20, pos.y + 20, pos.z + 20)
 
-minetest.register_on_leaveplayer(function(player) -- Remove player's soon-to-be-unused snow spawner
-	local name = player:get_player_name()
+	for i = 1, 7, 1 do
+		spawn_particle_inside_area(player, pos1, pos2)
+	end
+end
 
-	minetest.delete_particlespawner(particlespawners[name])
-	particlespawners[name] = nil
+local spawner_step = 0
+minetest.register_globalstep(function(dtime)
+	spawner_step = spawner_step + dtime
+
+	if spawner_step >= 0.7 then
+		spawner_step = 0
+
+		for _, player in pairs(minetest.get_connected_players()) do
+			spawn_particles(player)
+		end
+	end
 end)
