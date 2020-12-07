16c1944390d522a394d452f9237674bb58914a4c
LoneWolfHT <lonewolf04361@gmail.com>
2020-12-06 21:12:55 -0800

    Re-add a more careless version of snowfall

--- winterize/init.lua
+++ winterize/init.lua
@@ -3,8 +3,8 @@ local snowy_dirt_sounds = minetest.registered_nodes["default:dirt_with_snow"].so
 local grasses = {"dry_grass", "grass", "coniferous_litter",}
 local leaves = {"leaves", "aspen_leaves", "jungleleaves"}
 
---[[
-local snow_placement_blacklist = {"default:snow", "slab", "stair", "fence"}
+
+local snow_placement_blacklist = {"default:snow", "*slab*", "*stair*", "*fence*"}
 
 minetest.register_node("winterize:ice", { -- breaks instantly, drops nothing
 	drawtype = "nodebox",
@@ -34,7 +34,7 @@ minetest.register_node("winterize:ice", { -- breaks instantly, drops nothing
 minetest.register_lbm({
 	label = "Add ice layer to top of water",
 	name = "winterize:top_water_with_ice",
-	nodenames = {"default:water_source"},
+	nodenames = {"default:water_source", "default:river_water_source"},
 	run_at_every_load = true,
 	action = function(pos, node)
 		local pos_above = vector.new(pos.x, pos.y + 1, pos.z)
@@ -45,46 +45,50 @@ minetest.register_lbm({
 	end
 })
 
-local function snow_can_fall_freely(pos)
-	local voxelmanip = VoxelManip()
-	local aircheck_pos = vector.new(pos.x, pos.y + 20, pos.z)
-	local vpos1, vpos2 = voxelmanip:read_from_map(pos, aircheck_pos)
-	local voxelarea = VoxelArea:new{MinEdge=vpos1, MaxEdge=vpos2}
-	local nodes_above = voxelmanip:get_data()
+-- local function snow_can_fall_freely(pos)
+-- 	local voxelmanip = VoxelManip()
+-- 	local aircheck_pos = vector.new(pos.x, pos.y + 20, pos.z)
+-- 	local vpos1, vpos2 = voxelmanip:read_from_map(pos, aircheck_pos)
+-- 	local voxelarea = VoxelArea:new{MinEdge=vpos1, MaxEdge=vpos2}
+-- 	local nodes_above = voxelmanip:get_data()
 
-	for ypos = pos.y, aircheck_pos.y, 1 do
-		local nodeid = nodes_above[voxelarea:indexp(vector.new(pos.x, ypos, pos.z))]
-		if nodeid ~= minetest.CONTENT_AIR and
-		minetest.registered_items[minetest.get_name_from_content_id(nodeid)].pointable then -- ignore barriers
-			return false -- Obstruction found
-		end
-	end
+-- 	for ypos = pos.y, aircheck_pos.y, 1 do
+-- 		local nodeid = nodes_above[voxelarea:indexp(vector.new(pos.x, ypos, pos.z))]
+-- 		if nodeid ~= minetest.CONTENT_AIR and
+-- 		minetest.registered_items[minetest.get_name_from_content_id(nodeid)].pointable then -- ignore barriers
+-- 			return false -- Obstruction found
+-- 		end
+-- 	end
 
-	return true -- Snow can fall freely
-end
+-- 	return true -- Snow can fall freely
+-- end
+
+-- local function snow_can_fall_freely(pos)
+-- 	return not minetest.raycast(pos, vector.new(pos.x, pos.y+5, pos.z), false, true):next()
+-- end
 
 minetest.register_lbm({
 	label = "Place snow on top of nodes",
 	name = "winterize:top_nodes_with_snow",
-	nodenames = {"group:crumbly", "group:leaves", "group:cracky", "group:choppy"},
+	nodenames = {"group:crumbly", "group:leaves", "group:choppy"},
 	run_at_every_load = true,
 	action = function(pos, node)
 		local pos_above = vector.new(pos.x, pos.y + 1, pos.z)
 
 		for _, searchfor in pairs(snow_placement_blacklist) do
-			if node.name:find(searchfor) ~= nil then
+			if node.name:match(searchfor) ~= nil then
 				return
 			end
 		end
 
 		if minetest.get_node(pos_above).name == "air" and minetest.registered_items[node.name].walkable then
-			if snow_can_fall_freely(pos_above) then
+			-- if snow_can_fall_freely(pos_above) then
 				minetest.set_node(pos_above, {name = "default:snow"})
-			end
+			-- end
 		end
 	end
 })
-]]
+
 
 for _, leaftype in pairs(leaves) do
 	minetest.override_item("default:" .. leaftype, {
