c73b7b5736b604c96244be8c98a6b922b3f5bb90
LoneWolfHT <lonewolf04361@gmail.com>
2022-12-26 21:43:32 -0800

    Update init.lua

--- santa_hat_event/init.lua
+++ santa_hat_event/init.lua
@@ -1,4 +1,4 @@
-if os.date("%m") ~= "12" or tonumber(os.date("%d")) < 11 or not minetest.get_modpath("server_cosmetics") then return end
+if os.date("%m") ~= "12" or tonumber(os.date("%d")) < 11 or tonumber(os.date("%d")) > 24 not minetest.get_modpath("server_cosmetics") then return end
 
 local META_KEY = "server_cosmetics:snowballs:"..os.date("%Y")
 local COSMETIC_KEY = "server_cosmetics:entity:santa_hat:"..os.date("%Y")
